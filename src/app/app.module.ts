35927c0890332617235afb466fa8f90fbb54e596
Tich53 <rdouette@yahoo.fr>
2022-08-09 23:59:49 +0200

    Challenge 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ValidationButtonComponent } from './validation-button/validation-button.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { BlockComponentsComponent } from './block-components/block-components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValidationButtonComponent,
+    HeroesComponent,
+    BlockComponentsComponent
   ],
   imports: [
     BrowserModule,
