a6b82465afbaca7b6e8d1f08ea66243f9d85f98d
Tich53 <rdouette@yahoo.fr>
2022-08-15 15:20:28 +0200

    Challenge3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { ValidationButtonComponent } from './validation-button/validation-button.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BlockComponentsComponent } from './block-components/block-components.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ValidationButtonComponent,
     HeroesComponent,
-    BlockComponentsComponent
+    BlockComponentsComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
