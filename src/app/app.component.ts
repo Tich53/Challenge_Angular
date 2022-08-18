3dbf943d32ade30a670f453525879999e088c6a4
Tich53 <rdouette@yahoo.fr>
2022-08-18 17:10:38 +0200

    Challenge10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Richard';
+  title = "Richard";
 
-  isThisIngredientVital : boolean = true;
+  isThisIngredientVital: boolean = true;
 
   displayGuestList = false;
 
-  songList :string[] = ["Alejandro", "Ne me quitte pas", "Le temps est bon"];
+  songList: string[] = ["Alejandro", "Ne me quitte pas", "Le temps est bon"];
 
+  onomatopoeias?: string[] = ["bim", "bam", "boum"];
+
+  onReceiveNewOnomatopia(inputValue: string) {
+    this.onomatopoeias?.push(inputValue);
+  }
 }
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ValidationButtonComponent } from './validation-button/validation-button.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { BlockComponentsComponent } from './block-components/block-components.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MenuComponent } from './menu/menu.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { SearchMovieComponent } from './search-movie/search-movie.component';
-import { DeveloperComponentComponent } from './developer-component/developer-component.component';
-import { SkillComponentComponent } from './skill-component/skill-component.component';
-
-
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ValidationButtonComponent } from "./validation-button/validation-button.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { BlockComponentsComponent } from "./block-components/block-components.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MenuComponent } from "./menu/menu.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { SearchMovieComponent } from "./search-movie/search-movie.component";
+import { DeveloperComponentComponent } from "./Challenge9_Input/developer-component/developer-component.component";
+import { SkillComponentComponent } from "./Challenge9_Input/skill-component/skill-component.component";
+import { CreateOnomatopiaComponentComponent } from "./Challenge10_Output/create-onomatopia-component/create-onomatopia-component.component";
 
 @NgModule({
   declarations: [
@@ -29,14 +27,10 @@ import { SkillComponentComponent } from './skill-component/skill-component.compo
     SearchMovieComponent,
     DeveloperComponentComponent,
     SkillComponentComponent,
+    CreateOnomatopiaComponentComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
